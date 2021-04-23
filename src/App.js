e6f045970fbe947c29b3e703361ccf462063096f
marwinburesch <marwin@posteo.de>
2021-04-23 10:54:24 +0200

    Add generic content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">Hello, this might be a react app at some point</div>
+  );
 }
 
 export default App;
