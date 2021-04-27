2e935e4a1a170018ff32b1e7308b626d0c8b4535
marwinburesch <marwin@posteo.de>
2021-04-27 16:42:04 +0200

    Implement IconToggle and Logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
+import IconToggle from "./components/IconToggle";
+import Logo from "./components/Logo";
+import { ReactComponent as DocPlus } from "./icons/DocPlus.svg";
+import { ReactComponent as DocMinus } from "./icons/DocMinus.svg";
 
 function App() {
   return (
-    <div className='App'>Hello, this might be a react app at some point</div>
+    <div className='App'>
+      <h1>Tickets please!</h1>
+      <IconToggle componentA={<DocPlus />} componentB={<DocMinus />} />
+      <Logo size='s' />
+      <Logo size='m' />
+      <Logo size='l' />
+      <Logo size='xl' />
+      <button>make another button</button>
+    </div>
   );
 }
 
